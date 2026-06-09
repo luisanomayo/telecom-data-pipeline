818a7dc096016900bd89be12bfa8983457611ce1
Ame Avan-Nomayo <36841345+luisanomayo@users.noreply.github.com>
2026-06-09 22:06:50 +0100

    point airflow to sql folder

--- sql/setup/watermark_initial_insert.sql
+++ sql/setup/watermark_initial_insert.sql
@@ -1,5 +1,5 @@
 --seed initial watermarks for each source table
-INSERT INTO pipeline_watermark (source_table, last_processed)
+INSERT INTO pipeline_watermarks (source_table, last_processed)
 VALUES
     ('src_billing_transactions', '1970-01-01 00:00:00'),
     ('src_network_sessions', '1970-01-01 00:00:00'),
